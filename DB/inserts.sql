c11692e25e6a08c95c35be6f76115870ba18fc62
KurdulijaD <dejankurdulija00@gmail.com>
2022-05-28 14:32:42 +0200

    adding inserts for database

---
+++ DB/inserts.sql
@@ -0,0 +1,23 @@
+insert into korisnik values(1, 'Luka', 'Ciric', 'lukaciric', 'luka.ciric@yahoo.com');
+insert into korisnik values(2, 'Milorad', 'Markovic', 'mikipaok', 'mikipaok@yahoo.com');
+insert into korisnik values(3, 'Dejan', 'Kurdulija', 'dekikuki', 'kurdulijad@yahoo.com');
+insert into korisnik values(4, 'Zdravko', 'Milinkovic', 'kozdrav', 'milinkovicz@yahoo.com');
+
+
+insert into student values('PR108/2019', 'Nemanja', 'Malinovic', 'malina', 'nemanjamalinovic@yahoo.com', 3);
+insert into student values('PR12/2019', 'Jelena', 'Ilic', 'ilicka', 'ilicka00@gmail.com', 3);
+insert into student values('PR30/2019', 'Vladimir', 'Stanojevic', 'vlox', 'stanojeviccc@yahoo.com', 3);
+insert into student values('PR43/2019', 'Nikola', 'Bajagic', 'bajaga', 'bajagicnikola10@gmail.com', 3);
+insert into student values('PR62/2019', 'Aleksandar', 'Zaric', 'zare00', 'zaric00@yahoo.com', 3);
+insert into student values('PR15/2020', 'Katarina', 'Marinkovic', 'katarina', 'marinkovickaca@gmail.com', 2);
+insert into student values('PR22/2018', 'Milan', 'Mastilovic', 'mastamozesvasta', 'mastilovicmilan@yahoo.com', 4);
+insert into student values('PR12/2018', 'Nebojsa', 'Gordic', 'gorda', 'gordicn@yahoo.com', 4);
+
+insert into profesor values(1, 'Petar', 'Maric', 'zlipera', 'petar.maric@uns.ac.rs', 'ACS');
+insert into profesor values(2, 'Branislav', 'Atlagic', 'branislav', 'atlagic@uns.ac.rs', 'ESI');
+insert into profesor values(3, 'Marina', 'Stanojevic', 'marinas', 'marinas@uns.ac.rs', 'EEPSI');
+insert into profesor values(4, 'Bojana', 'Despotovic', 'despotovic', 'bojanadespotovic@uns.ac.rs', 'DEPT');
+insert into profesor values(5, 'Aleksandar', 'Erdeljan', 'erdeljan', 'aleksandar.erdeljan@uns.ac.rs', 'ESI');
+insert into profesor values(6, 'Luka', 'Strezovski', 'lukaaa', 'lukastrezovski@uns.ac.rs', 'EEPSI');
+insert into profesor values(7, 'Srdjan', 'Popov', 'jelovopopov', 'popovsrdjan@uns.ac.rs', 'ACS');
+insert into profesor values(8, 'Imre', 'Lendak', 'imre', 'lendak.imre@uns.ac.rs', 'ESI');
