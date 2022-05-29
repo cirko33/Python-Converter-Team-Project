8f5baf4bc33ec5760a4e09325be61d267c23d829
Kimara1904 <mikipaok@gmail.com>
2022-05-29 20:32:26 +0200

    Testing and Change Repository unit. Need to upgradegit add .!

--- Repository/Repository.py
+++ Repository/Repository.py
@@ -1,75 +1,33 @@
+from cmath import e
 from multiprocessing import connection
 from types import NoneType
 import mysql.connector
 from mysql.connector import Error
 from setuptools import find_namespace_packages
 
-class Repository:
-    def __init__(self):
-        connection = mysql.connector.connect(host="localhost", database="res", user="res_projekat", password="restim20")
+def read_items(query):
+    connection = mysql.connector.connect(host="localhost", database="res", user="res_projekat", password="restim20")
+    if connection.is_connected():
+        cursor = connection.cursor()
+        cursor.execute(query)
+        records = cursor.fetchall()
+        
+        cursor.close()
+        connection.close()
+        return records
+    else:
+        raise Error("Error in MySQL connection")
 
-    def execute_sql(self, query):
-        query1 = query.lower()
-        if "select" in query1:
-            self.__read_items(query)
-        else:
-            self.__execute_rest(query)   
+def execute_rest(query):
+    connection = mysql.connector.connect(host="localhost", database="res", user="res_projekat", password="restim20")
+    if connection.is_connected():
+        cursor = connection.cursor()
+        cursor.execute(query)
+        connection.commit()
+        retVal = cursor.rowcount
 
-    def __read_items(self, query):
-        try:
-            connection = mysql.connector.connect(host="localhost", database="res", user="res_projekat", password="restim20")
-            cursor = connection.cursor()
-            cursor.execute(query)
-            records = cursor.fetchall()
-
-            print("Number of rows: ", cursor.rowcount)
-
-            print("\nPrinting rows: ")
-
-            for row in records:
-                print("Id: ", row[0], " ")
-                print("Name: ", row[1], " ")
-                print("Last name: ", row[2], " ")
-                print("Username: ", row[3], " ")
-                print("Email: ", row[4], " ")
-
-                if "profesor" in query:
-                    print("Department = ", row[5], "\n")
-                elif "student" in query:
-                    print("Year of study = ", row[5], "\n")
-
-        except Error as e:
-            print("Error in MySQL", e)
-        finally:
-            if connection.is_connected():
-                cursor.close()
-                connection.close()
-                print("\nMySQL connection is closed!!!\n")
-
-    def __execute_rest(self, query):
-        try:
-            connection = mysql.connector.connect(host="localhost", database="res", user="res_projekat", password="restim20")
-            cursor = connection.cursor()
-            cursor.execute(query)
-            connection.commit()
-
-            print(cursor.rowcount, "row", end=" ")
-            if cursor.rowcount > 1:
-                print("s are ", end = " ")
-            else:
-                print(" is ", end = " ")
-            query1 = query.lower()
-            x = query1.split(" ")
-
-            if  "insert" in query1:
-                print(x[0] + "ed\n")
-            else:
-                print(x[0] + "d\n");
-
-        except Error as e:
-            print("Error in MySQL", e)
-        finally:
-            if connection.is_connected():
-                cursor.close()
-                connection.close()
-                print("\nMySQL connection is closed!!!\n")
+        cursor.close()
+        connection.close()
+        return retVal
+    else:
+        raise Error(msg="Error in MySQL connection")
---
+++ Repository/Repository_test.py
@@ -0,0 +1,18 @@
+from cmath import e
+from mysql.connector import Error
+from typing import Tuple
+import unittest
+
+from Repository import read_items
+from Repository import execute_rest
+
+class TestRepository(unittest.TestCase):
+    def test_succsess(self):
+        self.assertAlmostEqual(read_items("select * from korisnik;"), [(1, 'Luka', 'Ciric', 'lukaciric', 'luka.ciric@yahoo.com'), (2, 'Milorad', 'Markovic', 'mikipaok', 'mikipaok@yahoo.com'), (3, 'Dejan', 'Kurdulija', 'dekikuki', 'kurdulijad@yahoo.com'), (4, 'Zdravko', 'Milinkovic', 'kozdrav', 'milinkovicz@yahoo.com')])
+        self.assertAlmostEqual(execute_rest("insert into korisnik(id, name, lastname, username, email) values('6', 'mika', 'mikic', 'mikaslika', 'mik1@gmail.com')"), 1)
+        self.assertAlmostEqual(execute_rest("update korisnik set name = 'zika' where lastname='mikic';"), 1)
+        self.assertAlmostEqual(execute_rest("delete from korisnik where id=6;"), 1)
+
+    # def test_query_value(self):
+    #     self.assertRaises(Error, read_items, True)
+    #     self.assertRaises(Error, execute_rest, True)
