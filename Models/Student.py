d2a79ad38bf456b2f068e50dd95d61c6335ddc59
MilinkovicZ <zdravkolegenda123@gmail.com>
2022-05-30 12:03:33 +0200

    Fixing minor issues

--- Models/Student.py
+++ Models/Student.py
@@ -1,8 +1,8 @@
 class Student:
-    def __init__(self, id, name, lastname, username, email, yearOfStudy):
+    def __init__(self, id, name, lastname, username, email, year_of_study):
         self.id = id
         self.name = name
         self.lastname = lastname
         self.username = username
         self.email = email
-        self.yearOfStudy = yearOfStudy
+        self.year_of_study = year_of_study
