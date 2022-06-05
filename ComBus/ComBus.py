4479c639277f7ff8b691c028bcbd2f52ed255c76
cirko33 <luka.ciric2106@gmail>
2022-06-05 15:16:17 +0200

    Enabled data sending

--- ComBus/ComBus.py
+++ ComBus/ComBus.py
@@ -1,14 +1,42 @@
+from CheckFormat import *
 import sys
 sys.path.insert(0, "..")
 from Connection.Server import Server
 
-s = Server(8000)
-while True:
-    print("Client: " + s.receive())
+if __name__ == '__main__':
+    web_client_soc = Server(8000)
+    json_xml_soc = Server(8001)
+    xml_db_soc = Server(8002)
 
-    s.send(input("enter text to send"))
+    while True:
+        req = web_client_soc.receive()
+        print("Received request from Web Client and forward to JSONXMLAdapter: " + req )
+        json_xml_soc.send(req)
 
-    if input("Enter x for exit:") == "x":
-        break
+        xml_req = json_xml_soc.receive()
+        print("Request in XML form: " + xml_req)
 
-s.close()
+        check = check_xml_format(xml_req)
+        if check != "SUCCESS 2000":
+            print("Not valid request")
+            web_client_soc.send(check)
+            continue
+        
+        print("Forward request to XML DB Adapter")
+        xml_db_soc.send(xml_req)
+
+        res = xml_db_soc.receive()
+        print("Received response from XML DB Adapter: " + res)
+
+        print("Forward to JSON XML Adapter")
+        json_xml_soc.send(res)
+
+        json_res = json_xml_soc.receive()
+        print("Received response from JSON XML Adapter: " + json_res)
+        
+        print("Forward to Web Client")
+        web_client_soc.send(json_res)
+    
+    web_client_soc.close()
+    json_xml_soc.close()
+    xml_db_soc.close()
