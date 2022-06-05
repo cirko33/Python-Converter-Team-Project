cb893a136f766a46b7240baa8b5b2bd277af8189
Kimara1904 <mikipaok@gmail.com>
2022-06-05 22:45:05 +0200

    Fixing minor isues

--- ComBus/CheckFormat.py
+++ ComBus/CheckFormat.py
@@ -43,7 +43,7 @@ def check_xml_format(xml_request):
 
         #If there is any wrong character
         for char in xml_field:
-            if char is not isalnum() and char not in [";", " "]:
+            if not isalnum(char) and char not in [";", " "]:
                 raise ValueError("BAD_FORMAT 5000")
 
     return "SUCCESS 2000"
